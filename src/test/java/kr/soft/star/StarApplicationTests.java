3dfc59c687c7657cb36bd9e97d62bd85ad2d8c3b
pureum1215 <pureum1215@naver.com>
2025-08-01 10:40:56 +0900

    first commit

---
+++ src/test/java/kr/soft/star/StarApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.soft.star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
